2855f1809f78e1265544e2fe96cf608d82e46d48
Pavel Maslov <pavel.maslov@axfood.se>
2023-03-26 21:38:05 +0200

    add SNS topic with email subscription, random delay

--- consumer.js
+++ consumer.js
@@ -1,3 +1,26 @@
+const  { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
+const sns = new SNSClient({});
+
+
 exports.handler = async event => {
   console.log(event["Records"][0]["body"]);
+
+  try {
+    const res = await sns.send(new PublishCommand({
+      TopicArn: process.env.SNS_TOPIC_ARN,
+      Message: JSON.stringify(event["Records"][0]["body"])
+    }));
+
+    console.log(res.MessageId);
+    return {
+      statusCode: 200,
+      body: `message sent to SNS topic`
+    }
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: 'Failed to send message to SNS topic'
+    };
+  }
 }
--- producer.js
+++ producer.js
@@ -1,23 +1,33 @@
 const  { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
 const sqs = new SQSClient({});
 
+const getRandomIntInclusive = (min, max) => {
+  let minimum = Math.ceil(min);
+  let maximum = Math.floor(max);
+  return Math.floor(Math.random() * (maximum - minimum + 1) + minimum); // The maximum is inclusive and the minimum is inclusive
+};
 
 exports.handler = async event => {
-  let sqs_url = process.env.SQS_QUEUE_URL;
-  console.log(sqs_url);
   console.log(event);
+  let message = "hello world";
+  if (event.body) {
+    const gson = JSON.parse(event.body);
+    if (gson.message)
+      message = gson.message;
+  }
 
+  const random_seconds = getRandomIntInclusive(0, 60 * 5);
   try {
     const res = await sqs.send(new SendMessageCommand({
       QueueUrl: process.env.SQS_QUEUE_URL,
-      DelaySeconds: 60,
-      MessageBody: "hello world"
+      DelaySeconds: random_seconds,
+      MessageBody: message
     }));
 
     console.log(res.MessageId);
     return {
       statusCode: 200,
-      body: `message added to the Queue`
+      body: `message added to the Queue with a delay: ${random_seconds} sec`
     }
   }
   catch (err) {
